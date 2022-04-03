8d24684402318f599148b35f578281111ace1caf
NachoCabrera <ignacio.cabrera.garcia@gmail.com>
2022-04-03 19:54:35 +0200

    Commit inicial

---
+++ init_script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "npm init -y" && \
+npm init -y && \
+echo "npm i express" && \
+npm i express && \
+echo "npm i mongodb" && \
+npm i mongodb && \
+echo "npm i -D nodemon" && \
+npm i -D nodemon && \
+echo "npm i dotenv" && \
+npm i dotenv && \
+echo "npm install -D @babel/core @babel/preset-env @babel/node" && \
+npm install -D @babel/cli @babel/core @babel/preset-env && \
+echo "create babel.config.json" && \
+touch babel.config.json && \
+echo "{\"presets\": [\"@babel/preset-env\"] }" >> babel.config.json && \
+
+
+echo "Crear estructura de proyecto" && \
+mkdir src && cd src && \
+mkdir api && mkdir config && mkdir services && mkdir models && mkdir scripts && mkdir suscribers && mkdir routes && mkdir controllers && mkdir utils && mkdir middlewares && mkdir test && \
+touch app.js && \
+cd ../ && \
+
+echo "Git init" && \
+git init . && \
+touch .gitignore && \
+echo "node_modules/" >> .gitignore && \
+echo "disti/" >> .gitignore && \
+echo "package-lock.json" >> .gitignore 
+
+
